55cc8374843c08afc2fca3d7a44d52c997aa057f
yogson <yogson@gmail.com>
2024-01-27 18:35:14 +0400

    feat: refresh token

--- src/api/settings.py
+++ src/api/settings.py
@@ -8,7 +8,7 @@ PATH = f"/{PATH_PREFIX}/v{API_VER}"
 SECRET_KEY = environ.get("SECRET_KEY")
 APP_KEYS = environ.get("APP_KEYS").split(",")
 ALGORITHM = "HS512"
-ACCESS_TOKEN_TTL = 600
+ACCESS_TOKEN_TTL = 15
 REFRESH_TOKEN_TTL = 10800
 ORIGINS = environ.get("ORIGINS").split(",")
 
--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -31,22 +31,32 @@ async def login(request: Request, response: Response, form_data: Annotated[OAuth
 
 @auth_router.post("/register")
 @limiter.limit(LIMIT_REGISTER)
-async def register(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+async def register(request: Request, response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
     if form_data.client_secret not in settings.APP_KEYS:
         raise exceptions.unknown_client
     user_register = LocalUser(username=form_data.username).register(password=form_data.password)
     if not user_register:
         await sleep(1)
         raise exceptions.already_exists
-    return {"user": user_register.username}
+    user = LocalUserLoginProcessor(username=form_data.username).login(password=form_data.password)
+    if not user or user.disabled:
+        raise exceptions.bad_creds
+    token_processor = TokenProcessor(for_user=user)
+    response.set_cookie(
+        key="refresh_token", value=token_processor.get_refresh_token(), httponly=True, secure=False, samesite="lax"
+    )
+    return {"access_token": token_processor.get_access_token(), "token_type": "Bearer"}
 
 
 @auth_router.post("/refresh")
 @limiter.limit(LIMIT_REFRESH)
-async def refresh_token(request: Request, refresh_token: str = Cookie(None)):
+async def refresh_token(request: Request, response: Response, refresh_token: str = Cookie(None)):
     if refresh_token is None:
         raise no_refresh_token
     token_processor = TokenProcessor().verity_refresh_token(refresh_token)
     if not token_processor.user:
         raise bad_refresh_token
+    response.set_cookie(
+        key="refresh_token", value=token_processor.get_refresh_token(), httponly=True, secure=False, samesite="lax"
+    )
     return {"access_token": token_processor.get_access_token(), "token_type": "Bearer"}
